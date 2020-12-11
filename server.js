362d8210c13e7504b06038e9fcda14c3350eb3e7
LamiaHarbeg <harbeg.lamia@gmail.com>
2020-12-11 09:49:36 +0000

    try

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ app.use(express.json());
 // routes
 app.use('/api',authRouter)
 
+
 //serve static assets if in production
 if (process.env.Node_ENV === 'production'){
   //set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('./client/build'));
 
   app.get('*',(req,res)=>{
 res.sendFile(path.resolve(__dirname,'client','build','index.html'))
