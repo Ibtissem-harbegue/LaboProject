3f2c27e33241933e53b2493566405863bc3b62c3
LamiaHarbeg <harbeg.lamia@gmail.com>
2020-12-11 12:30:58 +0000

    make it better

--- config/connectDB.js
+++ config/connectDB.js
@@ -6,7 +6,7 @@ const config = require("config");
 const connectDB = async () => {
   try {
     mongoose.connect(
-      config.get("MONGO_URI"),
+      process.env.MONGODB_URI || config.get("MONGO_URI"),
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
